ba183602e91c23a52ef6f14985faf0844572d786
lixianyuan <861034458@qq.com>
2019-04-16 19:57:08 +0800

    线程安全性-原子性-atomicInteger/Long，及LongAdder的使用

---
+++ src/main/java/com/mmall/concurrency/example/atomic/AtomicExample2.java
@@ -0,0 +1,56 @@
+package com.mmall.concurrency.example.atomic;
+
+import com.mmall.concurrency.annoations.ThreadSage;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Slf4j
+@ThreadSage
+public class AtomicExample2 {
+
+    //请求总数
+    public static int clientTotal = 5000;
+
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static AtomicLong count = new AtomicLong(0);
+
+    public static void main(String[] args) throws Exception {
+
+        //线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //信号量
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    //log.error("exception",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        //关闭线程池
+        executorService.shutdown();
+        System.out.println("count:{}" + count);
+        // log.info("count:{}",count);
+    }
+
+    //这个方法是线程不安全的写法
+    private static void add() {
+        count.incrementAndGet();
+        //count.getAndIncrement();
+    }
+}
